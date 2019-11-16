870246707145cbc27256408213725ed522eeb7fd
Fabricio Parola <fabricio.parola@gmail.com>
2019-11-16 05:59:56 -0300

    Fix manage de propriedades

--- src/app/utils/PropertyUtils.js
+++ src/app/utils/PropertyUtils.js
@@ -2,9 +2,10 @@ const Moment = require('moment')
 
 const manageProperties = (properties) => {
   return properties.reduce((prev, curr) => {
+    console.log(Moment(new Date()).diff(curr.createdAt, 'days'))
     if (curr.in_promotion) {
       prev.promotions.push(curr)
-    } else if (Moment(new Date()).diff(curr.createdAt, 'days') >= process.env.NEW_PROPERTY_DAYS) {
+    } else if (Moment(new Date()).diff(curr.createdAt, 'days') <= Number(process.env.NEW_PROPERTY_DAYS)) {
       prev.new.push(curr)
     } else {
       prev.remaining.push(curr)
