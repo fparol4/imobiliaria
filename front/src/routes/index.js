19e56676fb5395ecd979ae810a7db9293f31184a
Brozinga <fbrozinga@outlook.com>
2019-11-24 21:52:21 -0300

    Adicionado sistema de Upload de imagem, pagina de Perfil, e de Proposta

--- front/src/routes/index.js
+++ front/src/routes/index.js
@@ -6,27 +6,35 @@ module.exports = app => {
 
   /* GET home page. */
   router.get('/', async (req, res, next) => {
-    res.render('index');
+    res.render('index', { logged: true, admin: true });
   });
 
   router.get('/informacoes', async (req, res, next) => {
-    res.render('pages/informations');
+    res.render('pages/informations', { logged: false, admin: false });
   });
 
   router.get('/cadastro', async (req, res, next) => {
-    res.render('pages/register', { login: false });
+    res.render('pages/register', { login: false, admin: false });
   });
 
   router.get('/login', async (req, res, next) => {
-    res.render('pages/register', { login: true });
+    res.render('pages/register', { login: true, admin: false });
   });
 
   router.get('/editor', async (req, res, next) => {
-    res.render('pages/editor');
+    res.render('pages/editor', { logged: true, admin: true });
   });
 
     router.get('/dashboard', async (req, res, next) => {
-    res.render('pages/dashboard');
+    res.render('pages/dashboard', {logged: true, admin: true });
+  });
+
+    router.get('/perfil', async (req, res, next) => {
+    res.render('pages/perfil', {logged: true, admin: false });
+  });
+
+      router.get('/propostas', async (req, res, next) => {
+    res.render('pages/propostas', {logged: true, admin: false });
   });
 
   app.use(router);
